6133a520f4efbe701f5f5ce0c73df72483124b4d
ByronMike <auger.michaell@gmail.com>
2024-08-09 14:55:20 +0200

    feat: add middleware, login and logout

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,23 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
-  // , the user will be redirected to our custom login page, rather than the NextAuth.js default page.
+  // The user will be redirected to our custom login page, rather than the NextAuth.js default page.
   pages: {
     signIn: "/login",
+  },
+  // The authorized callback is used to verify if the request is authorized to access a page via Next.js Middleware. It is called before a request is completed, and it receives an object with the auth and request properties. The auth property contains the user's session, and the request property contains the incoming request.
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
   },
   providers: [],
 } satisfies NextAuthConfig;
